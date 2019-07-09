086cfe91535486a7ce52aa638b69b4822180cd99
1432419543@qq.com <1432419543@qq.com>
2019-07-09 20:05:27 +0800

    Revert "vue"
    This reverts commit 0c91a1e8

--- minimall-admin-vue/src/App.vue
+++ minimall-admin-vue/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <router-view />
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
@@ -9,3 +10,14 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
