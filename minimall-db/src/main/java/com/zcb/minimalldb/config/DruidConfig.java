6ec778e1bbaafda214497dd4b683283e27a2c832
1432419543@qq.com <1432419543@qq.com>
2019-08-28 23:37:32 +0800

    druid update

--- minimall-db/src/main/java/com/zcb/minimalldb/config/DruidConfig.java
+++ minimall-db/src/main/java/com/zcb/minimalldb/config/DruidConfig.java
@@ -1,21 +1,16 @@
 package com.zcb.minimalldb.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,23 +19,22 @@ import java.util.Map;
  * @author zcbin
  * @title: DruidConfig
  * @projectName minimall
- * @description: TODO
+ * @description: druid连接池配置
  * @date 2019/8/27 19:58
  */
 @Configuration
-//@EnableAutoConfiguration(exclude={DruidDataSourceAutoConfigure.class})
 
 public class DruidConfig {
-		// 将所有前缀为spring.datasource下的配置项都加载到DataSource中
-		@Autowired
-		private DruidProties druidProties;
-		//@ConfigurationProperties(prefix = "spring.datasource")
-		//@Bean
+		/**
+		 * 参考官网配置
+		 * https://github.com/alibaba/druid/tree/master/druid-spring-boot-starter
+		 * @return
+		 */
+		@ConfigurationProperties(prefix = "spring.datasource.druid")
+		@Bean
 		public DataSource dataSource() {
-				System.out.println("----"+druidProties.getUrl());
-				return new DruidDataSource();
+				return  DruidDataSourceBuilder.create().build();
 		}
-		private static final String DB_PREFIX = "spring.datasource";
 		@Bean
 		public ServletRegistrationBean druidStatViewServlet() {
 				ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
@@ -61,46 +55,6 @@ public class DruidConfig {
 				filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
 				return filterRegistrationBean;
 		}
-		//解决 spring.datasource.filters=stat,wall,log4j 无法正常注册进去
-	//	@Component
-		//@ConfigurationProperties(prefix = DB_PREFIX)
-//		class IDataSourceProperties {
-//
-//
-//				//@Bean     //声明其为Bean实例
-//				//@Primary  //在同样的DataSource中，首先使用被标注的DataSource
-//				public DataSource dataSource() {
-//						System.out.println("url="+url);
-//						System.out.println("maxActive"+maxActive);
-//						DruidDataSource datasource = new DruidDataSource();
-//						datasource.setUrl(url);
-//						datasource.setUsername(username);
-//						datasource.setPassword(password);
-//						datasource.setDriverClassName(driverClassName);
-//
-//						//configuration
-//						datasource.setInitialSize(initialSize);
-//						datasource.setMinIdle(minIdle);
-//						datasource.setMaxActive(maxActive);
-//						datasource.setMaxWait(maxWait);
-//						datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-//						datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-//						datasource.setValidationQuery(validationQuery);
-//						datasource.setTestWhileIdle(testWhileIdle);
-//						datasource.setTestOnBorrow(testOnBorrow);
-//						datasource.setTestOnReturn(testOnReturn);
-//						datasource.setPoolPreparedStatements(poolPreparedStatements);
-//						datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
-//						try {
-//								datasource.setFilters(filters);
-//						} catch (SQLException e) {
-//								System.err.println("druid configuration initialization filter: " + e);
-//						}
-//						datasource.setConnectionProperties(connectionProperties);
-//						return datasource;
-//				}
-//
-//
-//		}
+
 
 }
