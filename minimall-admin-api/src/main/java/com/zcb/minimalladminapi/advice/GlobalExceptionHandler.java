22443ae204fee4f1f3f5e09c179a2bddeedd9918
1432419543@qq.com <1432419543@qq.com>
2019-08-29 16:22:42 +0800

    '统一异常处理'

---
+++ minimall-admin-api/src/main/java/com/zcb/minimalladminapi/advice/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.zcb.minimalladminapi.advice;
+
+import com.alibaba.fastjson.JSONObject;
+import com.zcb.minimallcore.util.ResponseUtil;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authz.UnauthenticatedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @author zcbin
+ * @title: GlobalExceptionHandler
+ * @projectName minimall
+ * @description: 统一异常处理类
+ * @date 2019/8/29 14:45
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+		@ExceptionHandler(value = UnauthenticatedException.class)
+		@Log(desc = "会话超时", clazz = GlobalExceptionHandler.class)
+		public JSONObject unauthenticatedException(UnauthenticatedException e) {
+				return ResponseUtil.unloginTimeOut();
+		}
+		@ExceptionHandler(value = Exception.class)
+		@Log(desc = "系统异常", clazz = GlobalExceptionHandler.class)
+		public JSONObject exception(Exception e) {
+				return ResponseUtil.serious();
+		}
+
+}
