ae3ff76d0a791dae2048f34df2b1acd82db92868
zcbin <1432419543@qq.com>
2019-10-16 15:07:08 +0800

    配置

--- minimall-admin-api/src/main/java/com/zcb/minimalladminapi/config/WebMvcConfig.java
+++ minimall-admin-api/src/main/java/com/zcb/minimalladminapi/config/WebMvcConfig.java
@@ -4,8 +4,8 @@ import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.zcb.minimalladminapi.annotation.support.LoginUserHandlerMethodArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -37,16 +37,34 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
     /**
-     * 格式化fastjson日期返回值格式
-     * @return
+     * 配置消息转换器
+     * @param converters
      */
-    @Bean
-    FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        //1.需要定义一个convert转换消息的对象;
+//        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+//        //2.添加fastJson的配置信息，比如：是否要格式化返回的json数据;
+//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+//        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat,
+//                SerializerFeature.WriteMapNullValue,
+//                SerializerFeature.WriteNullStringAsEmpty,
+//                SerializerFeature.DisableCircularReferenceDetect,
+//                SerializerFeature.WriteNullListAsEmpty,
+//                SerializerFeature.WriteDateUseDateFormat);
+//        //3处理中文乱码问题
+//        List<MediaType> fastMediaTypes = new ArrayList<>();
+//        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+//        //4.在convert中添加配置信息.
+//        fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
+//        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         FastJsonConfig config = new FastJsonConfig();
         config.setDateFormat("yyyy-MM-dd HH:mm:ss");
         converter.setFastJsonConfig(config);
-        return converter;
+        //5.将convert添加到converters当中.
+        converters.add(converter);
     }
+
 
 }
